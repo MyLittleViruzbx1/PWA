75453ae82ae523771b44a3639632f47d79d3ef8b
Myz <mylittleviruz@outlook.es>
2024-03-25 23:45:43 -0600

    agregado el event.respondWith para las peticiones del sw

--- 02-service-worker/myz.js
+++ 02-service-worker/myz.js
@@ -1,4 +1,17 @@
 self.addEventListener('fetch', event=>{
-    console.log(event);
-    console.log('first')
+    
+    // console.log(event);
+
+    if (event.request.url.includes('.jpg')){
+
+        // let fotoreq = fetch('img/main.jpg');
+        // let fotoreq = fetch(event.request.url);
+        let fotoreq = fetch(event.request);
+            event.respondWith( fotoreq );
+    }
+        // else {
+    //     event.respondWith( fetch(event.request) );
+    // }
+
+
 });
